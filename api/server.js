f23a2df535f82446dfa5014429d4a170349a6c1a
codecookride <57050292+codecookride@users.noreply.github.com>
2020-05-20 20:02:39 -0700

    doneso

--- api/server.js
+++ api/server.js
@@ -1,9 +1,18 @@
 const express = require("express");
 
+const Router = require("./router");
+
 const db = require("../data/dbConfig.js");
 
 const server = express();
 
 server.use(express.json());
+
+server.use("/api/accounts", Router);
+
+server.get("/", (req, res) => {
+    res.status(200).json({ api: "up" });
+  });
+  
 
 module.exports = server;
